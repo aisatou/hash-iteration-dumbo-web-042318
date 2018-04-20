2a4fc7d8c75163699177b5f41d7d308982e6ee08
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-20 23:42:04 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,12 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.fetch(:suite_a)
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner  
 end
